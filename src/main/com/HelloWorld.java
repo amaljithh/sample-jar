d7969f26a93642dfddc1e3eb3eaaada384491f25
root <root@ip-172-31-13-92.ap-south-1.compute.internal>
2024-05-09 09:41:39 +0000

    first commit

---
+++ src/main/com/HelloWorld.java
@@ -0,0 +1,9 @@
+// src/main/java/com/example/HelloWorld.java
+
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
